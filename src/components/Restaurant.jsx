bdb37f7e3ce1ae484e881e2f711970bc5d4ec2b7
Kai Lin Zhang <kaizen.agility@gmail.com>
2017-11-09 21:40:27 -0500

    Changed data on Restaurant.jsx to props from Splash API

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -6,26 +6,24 @@ const Restaurant = props => {
             <div className="main">
                 <div className="image" 
                     style={{ backgroundImage:
-                        'url(https://img.buzzfeed.com/buzzfeed-static/static/2015-07/16/12/enhanced/webdr06/enhanced-32098-1437062439-1.png?crop=590:390;0,55&amp;downsize=715:*&amp;output-format=auto&amp;output-quality=auto)',
+                        'url(${props.restaurant.image})'
                     }}
                 />
                 <div className="info">
-                    <h3>Gusteau's</h3>
+                    <h3>{props.restaurant.name}</h3>
                     <div className="meta">
-                        <span className="media">Ratatouille</span>
-                        <span className="media-category">Movie</span>
-                        <span className="category">Fine Dining</span>
+                        <span className="media">{props.restaurant.media}</span>
+                        <span className="media-category">{props.restaurant.media_category}</span>
+                        <span className="category">{props.restaurant.category}</span>
                     </div>
                     <div className="description">
                         <p>
-                            A Parisian bistro...a gourmet chef...
+                            {props.restaurant.description}
                         </p>
                     </div>
                 </div>
             </div>
-        <p className="attr">
-            Description from "28 Fictional Restaurants That Should Actually Be Real" By Mallory McInnis.https : //www.buzzfeed.com/mallorymcinnis/i-want-to-get-drunk-at-paddys-while-eating-a-bluth-banana
-        </p>
+            <p className="attr">{props.attr}</p>
         </div>
     );
 };
