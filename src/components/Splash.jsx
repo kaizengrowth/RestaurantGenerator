53a24783bbbc6ce7399f2af5be2ba694c1bffb78
Kai Lin Zhang <kaizen.agility@gmail.com>
2017-11-09 20:45:50 -0500

    Created Components folder and Splash.jsx

---
+++ src/components/Splash.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Splash extends Component {
+    render () {
+        return (
+            <div className="top">
+                <div className="introtext">
+                    <div className="emoji">
+                        <p>😱</p>
+                    </div> 
+                    <div className="splashtext">
+                        <h1>Today, the Thundercats are lunching at...</h1>
+                    </div>
+                    <Restaurant />
+                    <div className="new-restauraunt-button">
+                        <span className="button">
+                            Get another restaurant!
+                        </span>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Splash;
